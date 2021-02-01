2192c6c8e5d68291bff63ac0bd2669bd1726cea6
Mahekb <51280331+Mahekb@users.noreply.github.com>
2021-02-01 20:39:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Matrix from './components/Matrix'
 import Navbar from './components/Navbar';
+import './index.css';
 
 ReactDOM.render(
   <>
   <Navbar></Navbar>
+  <Matrix/>
   </>,
   document.getElementById('root')
 );
+
