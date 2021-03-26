90a57ea07948132d6d9cd8b65a1f65d8fb5d6500
Mahekb <51280331+Mahekb@users.noreply.github.com>
2021-03-26 10:03:20 +0530

    Update Matrix.js

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -1,30 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect ,useState} from "react";
 import "./Matrix.css";
 import Node from "./Node";
+import "./Node.css";
+import dijkstra,{getNodesInShortestPathOrder} from "../algorithms/dijkstra";
+import "./index.css";
+import Button from "./Button"; 
+import Heading from "./Heading";
+import Dropdown from "./Dropdown"
 
-const getInitialGrid = () => {
-  const grid = [];
+function getInitialGrid () {
+  const grid=[];
   for (let row = 0; row < 15; row++) {
-    const currentRow = [];
+    const rowgri=[];
     for (let col = 0; col < 40; col++) {
-      currentRow.push(createNode(col, row));
+      rowgri.push(createNode(col, row));
     }
-    grid.push(currentRow);
+    grid.push(rowgri);
   }
   return grid;
 };
 
 function Matrix() {
-  const [grid, setgrid] = useState([]);
-  useEffect(() => {
-    const grid1 = getInitialGrid();
-    setgrid(grid1);
-  });
+  const [grid1, setgrid] = useState([]);
+  useEffect(()=>{
+    const grid2=getInitialGrid();
+    setgrid(grid2);
+  },[]);
   return (
     <>
+      <div className="navbar">
+                <Heading name="Path Visualization"></Heading>
+            <ul>
+                <Dropdown name="Algorithm"></Dropdown>
+                <Dropdown name="Maze and pattern"></Dropdown>
+                
+                <button onClick={()=>visualizeDijkstra(grid1)}>Start</button>
+                <Button title="Stop!"></Button>
+            </ul>
+        </div>
       <center>
         <div className="grid">
-          {grid.map((row, rowIdx) => {
+          {grid1.map((row, rowIdx) => {
             return (
               <div key={rowIdx}>
                 {row.map((node, nodeIdx) => {
@@ -60,5 +76,41 @@ const createNode = (col, row) => {
     distance: Infinity,
     isVisited: false,
     isWall: false,
+    previousNode: null,
   };
 };
+
+function animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+  for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+    if (i === visitedNodesInOrder.length) {
+      setTimeout(() => {
+        animateShortestPath(nodesInShortestPathOrder);
+      }, 10 * i);
+      return;
+    }
+    setTimeout(() => {
+      const node = visitedNodesInOrder[i];
+      document.getElementById(`node-${node.row}-${node.col}`).className =
+        'node-visited';
+    }, 10 * i);
+  }
+}
+
+function animateShortestPath(nodesInShortestPathOrder) {
+  for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+    setTimeout(() => {
+      const node = nodesInShortestPathOrder[i];
+      document.getElementById(`node-${node.row}-${node.col}`).className =
+        'node-shortest-path';
+    }, 50 * i);
+  }
+}
+
+function visualizeDijkstra(grid1) {
+  console.log("run");
+  const startNode = grid1[6][10];
+  const finishNode = grid1[7][13];
+  const visitedNodes  = dijkstra(grid1, startNode, finishNode);
+  const nodesInShortestPathOrder =  getNodesInShortestPathOrder(finishNode,startNode)
+  animateDijkstra(visitedNodes, nodesInShortestPathOrder);
+}
